ba3e1788e6d0137a7f0506a7081c1c971808b905
Carolyn Wang <carolynwang@college.harvard.edu>
2021-03-17 00:16:09 -0400

    hm

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,51 +1,90 @@
 var unirest = require("unirest");
+const fetch = require("node-fetch");
 var fs = require('fs');
-var req = unirest("GET", "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi");
-var dataToWrite = [];
-var counter = 0;
+// var dataToWrite = [];
+// var counter = 0;
 
+// const pulls = 2
 
+// for (let i=1; i < pulls+1; i++) {
+//     var req = unirest("GET", "https://unogsng.p.rapidapi.com/search");
+//     const offset = JSON.stringify(i*100-100)
+//     console.log(offset)
+//     req.query({
+//         start_year: '1972',
+//         orderby: 'rating',
+//         audiosubtitle_andor: 'and',
+//         start_rating: '7',
+//         limit: '100',
+//         end_rating: '10',
+//         subtitle: 'english',
+//         countrylist: '78',
+//         country_andorunique: 'unique',
+//         offset: offset,
+//         end_year: '2021'
+//     });
 
-for (let i=1; i < 6; i++) {
-
-    req.query({
-        "q": "get:new[25550]-!1950,2021-!0,5-!8,10-!0-!Any-!Any-!Any-!gt100-!{downloadable}",
-        "t": "ns",
-        "cl": "all",
-        "st": "adv",
-        "ob": "Title",
-        "p": JSON.stringify(i),
-        "sa": "and"
-    });
+//    // requests are definitely both going out, however, currently only the first one is coming back and being concatted "pull" number of times
     
-    req.headers({
-        "x-rapidapi-key": "",
-        "x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
-        "useQueryString": true
-    });
+//     req.headers({
+//         "x-rapidapi-key": "5ce2fa4156mshb085f498a38e5a0p126d5fjsna55d380ca794",
+//         "x-rapidapi-host": "unogsng.p.rapidapi.com",
+//         "useQueryString": true
+//     });
     
+//     req.end(function (res) {
+//         if (res.error) throw new Error(res.error);
+//         const data = res.body;
+
+//         console.log(data)
     
-    req.end(function (res) {
-
-        if (res.error) throw new Error(res.error);
-        const data = JSON.parse(res.body);
-
-        console.log(typeof data['ITEMS'])
-        dataToWrite = dataToWrite.concat(data['ITEMS']);
-        console.log(dataToWrite)
-        counter = counter + 1
-        console.log(counter)
-
-        if (counter === 5) {
-            console.log(dataToWrite)
-            fs.writeFileSync('./database.json',JSON.stringify(dataToWrite), function(err, result) {
-                console.log(result, err)
-                if(err) throw new Error(result.error);
-            })
-        }
+//         dataToWrite = dataToWrite.concat(data['results']);
+//         counter = counter + 1
+//         console.log(counter)
+    
+//         if (counter === pulls) {
+//             console.log('writing',counter)
+//             fs.writeFileSync('./database.json',JSON.stringify(dataToWrite), function(err, result) {
+//                 console.log(result, err)
+//                 if(err) throw new Error(result.error);
+//             })
+//         }
         
-    });
+//     });
+    
+// }
 
-}
+// when request is received
 
-console.log('written')
+
+var fetchPromise = fetch("https://unogsng.p.rapidapi.com/search?start_year=1972&orderby=rating&audiosubtitle_andor=and&limit=100&subtitle=english&countrylist=78%2C46&audio=english&country_andorunique=unique&offset=0&end_year=2019", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "",
+		"x-rapidapi-host": "unogsng.p.rapidapi.com"
+	}
+}).catch(err => {
+	console.error(err);
+});
+
+var jsonPromise = fetchPromise.then(response => {
+	return response.json();
+}).catch(err => {
+	console.error(err);
+});
+
+jsonPromise.then(json => console.log(json)).catch(err => {
+	console.error(err);
+});
+
+console.log('end!')
+
+/*
+ var response = await fetch(...);
+ var json = await response.json();
+ console.log(json)
+ console.log('end!')
+ */
+
+
+
